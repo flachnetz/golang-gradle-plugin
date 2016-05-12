3f8ad7ee895df34dd12d67a991188c5ae5a2f110
Oliver Bestmann <obest@esailors.de>
2016-05-12 08:49:07 +0000

    Add test directory

---
+++ test/lib/hello.go
@@ -0,0 +1,7 @@
+package lib
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello World")
+}
---
+++ test/main.go
@@ -0,0 +1,7 @@
+package main // import "srv-git-01-hh1.alinghi.tipp24.net/iwg/gradle-golang/test"
+
+import "srv-git-01-hh1.alinghi.tipp24.net/iwg/gradle-golang/test/lib"
+
+func main() {
+	lib.Hello()
+}
