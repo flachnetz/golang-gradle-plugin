5d75c4ca8508fdc5b72f7842ef174f94a3d5f89a
Martin Joehren <m.joehren@gmail.com>
2016-08-05 10:17:54 +0000

    playing around with imports

--- src/main/groovy/de.flachnetz.golang/GoPlugin.groovy
+++ src/main/groovy/de.flachnetz.golang/GoPlugin.groovy
@@ -3,7 +3,9 @@ package de.flachnetz.golang
 import java.nio.file.*
 import java.security.*
 import java.time.*
+import java.time.LocalDateTime
 import java.time.format.*
+import java.time.format.DateTimeFormatter
 import org.gradle.api.*
 import org.gradle.api.tasks.*
 
